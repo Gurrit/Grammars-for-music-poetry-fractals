6876743fe5ae0591619c07ea57f0edc50fa28a3c
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2019-02-26 16:20:02 +0100

    initialized Flask sockets

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,11 @@
 from turtle import *
 from flask import Flask, flash, url_for, send_from_directory
+from flask_socketio import SocketIO, emit
 
 # from src.backend.init import init
 
 app = Flask(__name__)
-
+socket = SocketIO(app)
 
 @app.route('/')
 def home():
@@ -12,8 +13,9 @@ def home():
     <!doctype html>
     <title>Grammar</title>
     <h1>choose grammar</h1>
+    <script> 
     '''
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000)
+    socket.run(app, host='0.0.0.0', port=4000)
