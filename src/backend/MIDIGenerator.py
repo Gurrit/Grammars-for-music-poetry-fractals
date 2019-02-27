d54cf889913eb13cd7da1dccdb20c27203bb60e2
sk8eddies <sk8eddie97@gmail.com>
2019-02-27 11:53:18 +0100

    STARTED with merging the music generation with the Parser. EM

---
+++ src/backend/MIDIGenerator.py
@@ -0,0 +1,71 @@
+import random
+import math
+from midiutil import MIDIFile
+
+
+class MIDIGenerator:
+
+    def __init__(self):
+        self.track = 0
+        self.channel = 0
+        self.time = 0  # In beats
+        self.duration = 1  # In beats
+        self.tempo = 120  # In BPM
+        self.volume = 100  # 0-127, as per the MIDI standard
+        self.key = 0  # c=0, c#=1, d=2 etc.
+        self.pitch = 60 + self.key
+        self.MyMIDI = MIDIFile(20)  # 20 tracks are allowed
+        self.MyMIDI.addTempo(self.track, self.time, self.tempo)
+        self.direction = 0
+
+    def create_midi_file(self):
+        # Create the file
+        with open("turtle.mid", "wb") as output_file:
+            self.MyMIDI.writeFile(output_file)
+
+    def add_to_track(self, command):
+
+        command_split = command.split(":")
+
+
+        if command == "f":
+            if self.direction == 1:  # Raise the pitch
+                # If-satserna nedan definerar skalan
+                # if ((self.pitch-4)%12 == self.key or (self.pitch+1))%12 == self.key: # dur
+                if (self.pitch - 2) % 12 == self.key or (self.pitch + 1) % 12 == self.key or (
+                        self.pitch - 7) % 12 == self.key:  # harmonisk moll
+                    self.pitch = self.pitch + 1
+                elif (self.pitch - 8) % 12 == self.key:  # harmonisk moll
+                    self.pitch = self.pitch + 3
+                else:
+                    self.pitch = self.pitch + 2
+            elif self.direction == 3:
+                # if (self.pitch-5)%12 == self.key or self.pitch%12 == self.key: #dur
+                if (self.pitch - 3) % 12 == self.key or (self.pitch) % 12 == self.key or (
+                        self.pitch - 8) % 12 == self.key:  # harmonisk moll
+                    self.pitch = self.pitch - 1
+                elif (self.pitch + 1) % 12 == self.key:  # harmonisk moll
+                    self.pitch = self.pitch - 3
+                else:
+                    self.pitch = self.pitch - 2
+            else:
+                # if self.pitch > 60 + 12 + self.key:
+                if self.time > 4 * 32:  # One measure is 4 beats
+                    self.pitch = 60 + self.key
+                    self.time = 0
+                    self.track = self.track + 1
+                    self.MyMIDI.addTempo(self.track, self.time, self.tempo)
+                    print("new track")
+                # if self.pitch < 60 - 12 + self.key:
+                #    self.pitch = 60 + self.key
+                #    self.time = 0
+                #    self.track = self.track +1
+                #    self.MyMIDI.addTempo(self.track, self.time, self.tempo)
+                #    print("new track")
+                self.duration = math.pow(2, random.randint(-2, 2))  # Randomise notelength
+                self.MyMIDI.addNote(self.track, self.channel, self.pitch, self.time, self.duration, self.volume)
+                self.time = self.time + self.duration
+        elif command_split[0] == "r":
+            self.direction = (self.direction - 1) % 4
+        elif command_split[0] == "l":
+            self.direction = (self.direction + 1) % 4
--- src/backend/Parser.py
+++ src/backend/Parser.py
@@ -1,6 +1,7 @@
 from init import *
 from GFFileReader import *
 from config import *
+from MIDIGenerator import *
 import turtle
 
 
@@ -9,6 +10,21 @@ class Parser:
     def __init__(self, turtle):
         self._turtle = turtle
         self._turtle_map = {}
+
+    def parse(self):
+        pass
+
+    def parser_for_midi(self):
+        generator = MIDIGenerator()
+
+        file_reader = GFFileReader()
+        commands = file_reader.read_gf_file()
+
+        for command in commands:
+            generator.add_to_track(command)
+
+        generator.create_midi_file()
+
 
     def parse_for_turtle(self):
         init.get_instance().set_turtle(self._turtle)
@@ -27,6 +43,8 @@ class Parser:
                 self._turtle_map.get(command_split[0])(int(angle))
 
         turtle.done()
+
+        self.parser_for_midi()
 
 
 parser = Parser(turtle.Turtle())
