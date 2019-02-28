f524f6dfc138e397f536727a17f7d73f792f045b
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2019-02-28 14:56:58 +0100

    add sending and receiving drawing information via a websocket

--- src/backend/app.py
+++ src/backend/app.py
@@ -5,7 +5,7 @@ import websockets
 async def echo(websocket, path):
     async for message in websocket:
         print("got here")
-        await websocket.send(message)
+        await websocket.send("forward(50)")
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(echo, 'localhost', 8765))
--- src/frontend/draw.js
+++ src/frontend/draw.js
@@ -7,6 +7,12 @@ function connectToServer() {
     let url = settings.url;
     var socket = new WebSocket(url);
     console.log("Connected to server at " + url);
+    let turtle = new CreateTurtle(document.getElementById("canvas"));
+    turtle.forward(50);
+    socket.onmessage = function (event) {
+        console.log(event.data);
+        eval("turtle." + event.data)
+    }
     return socket;
 }
 
