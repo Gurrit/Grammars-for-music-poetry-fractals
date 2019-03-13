ca8aeb0067fad4248e660ce38a85a7053af1b88e
sk8eddies <sk8eddie97@gmail.com>
2019-03-13 16:01:08 +0100

    ADAPTED to koch curve. Trying to fix bug when not having tones for colours/angles. EM

--- piano.py
+++ piano.py
@@ -1,9 +1,9 @@
 # from browser import document, html
 import turtle
  
-gf_string = "B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B r A r B r A l B l A l B l A r B r A r B l A l B r A r B r A r B l A l B l A r B r A l B l A l B l A r B r A l B l A l B r A r B r A r B l A l B"
-rightAngle = 60
-leftAngle = 60
+gf_string = "F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F r F r F l F l F r F r F r F l F l F r F l F r F l F l F r F l F r F l F l F r F r F r F l F l F r F"
+rightAngle = 90
+leftAngle = 90
 colorArray = []
 rightAngleArray = []
 leftAngleArray = []
@@ -50,7 +50,7 @@ def interprete_notes(noteArray):
         elif (note == "b"):
             colorArray.append("#00FF80") #turqouise
         if (note == "highc"):
-            rightAngleArray.append(rightAngle + 2)
+            rightAngleArray.append(rightAngle + 29)
         elif (note == "highc#"):
             colorArray.append("#00FFFF") #baby blue
         elif (note == "highd"):
@@ -58,11 +58,11 @@ def interprete_notes(noteArray):
         elif (note == "highd#"):
             colorArray.append("#0080FF") #bright blue
         elif (note == "highe"):
-            rightAngleArray.append(rightAngle - 3)
+            rightAngleArray.append(rightAngle - 20)
         elif (note == "highf"):
             colorArray.append("#0000FF") #blue
         elif (note == "highf#"):
-            leftAngleArray.append(leftAngle - 3)
+            leftAngleArray.append(leftAngle - 20)
         elif (note == "highg"):
             colorArray.append("#7F00FF") #purple
         elif (note == "highg#"):
@@ -75,53 +75,61 @@ def interprete_notes(noteArray):
             colorArray.append("#FF007F") #magenta
 
 def go_turtle(grammarArrayen):
-    #print("doing the turtle")
- 
+
     brad = turtle.Turtle()
     brad.hideturtle()
     brad.speed(0)
-    # brad.penup()                #don't draw when turtle moves
-    # brad.goto(-400, 190)       #move the turtle to a location
-    # brad.pendown() 
 
     gf_commands = gf_string.split(" ")
+    gf_commands_length = len(gf_commands)
 
     numberOfColors = len(colorArray)
     numberOfRightAngles = len(rightAngleArray)
     numberOfLeftAngles = len(leftAngleArray)
-    colorSteps = len(gf_commands)/numberOfColors + len(gf_commands)%numberOfColors
-    rightAngleSteps = len(gf_commands)/numberOfRightAngles + len(gf_commands)%numberOfRightAngles
-    leftAngleSteps = len(gf_commands)/numberOfLeftAngles + len(gf_commands)%numberOfLeftAngles
+
+    colorSteps = gf_commands_length
+    rightAngleSteps = gf_commands_length
+    leftAngleSteps = gf_commands_length
+
+    if numberOfColors != 0:
+        colorSteps = gf_commands_length/numberOfColors + gf_commands_length % numberOfColors
+
+    if numberOfRightAngles != 0:
+        rightAngleSteps = gf_commands_length/numberOfRightAngles + gf_commands_length % numberOfRightAngles
+    if numberOfLeftAngles != 0:
+        leftAngleSteps = gf_commands_length/numberOfLeftAngles + gf_commands_length % numberOfLeftAngles
 
     colorIndex = 0
     rightAngleIndex = 0
     leftAngleIndex = 0
 
+    right = 90
+    left = 90
+
     brad.fill(True)
-    for i in range(0, len(gf_commands), 1):
-        if i%colorSteps==0:
+    for i in range(0, gf_commands_length, 1):
+        if (i % colorSteps == 0) and (colorSteps != gf_commands_length):
             brad.fill(False)
             brad.color(colorArray[colorIndex])
             colorIndex += 1
             brad.fill(True)
-        if i%rightAngleSteps==0:
-            rightAngle = rightAngleArray[rightAngleIndex]
+        if (i % rightAngleSteps == 0) and (rightAngleSteps != gf_commands_length):
+            print("HHELLO")
+            right = rightAngleArray[rightAngleIndex]
             rightAngleIndex += 1
-        if i%leftAngleSteps==0:
-            leftAngle = leftAngleArray[leftAngleIndex]
+        if (i % leftAngleSteps == 0) and (leftAngleSteps != gf_commands_length):
+            left = leftAngleArray[leftAngleIndex]
             leftAngleIndex += 1
-        
-        if gf_commands[i] == "A":
-            brad.forward(3)
-        elif gf_commands[i] == "B":
+
+        if (gf_commands[i] == "A") or (gf_commands[i] == "B") or (gf_commands[i] == "F"):
             brad.forward(3)
         elif gf_commands[i] == "r":
-            brad.right(rightAngle)
+            brad.right(right)
         elif gf_commands[i] == "l":
-            brad.left(leftAngle)
+            brad.left(left)
 
-    brad.fill(False)     
- 
+    brad.fill(False)
+
     turtle.done()
  
-main("c,e,f,a#,higha,highc#,highg#,higha#")
+main("c#,d#,g,highe,highf#, highc")
