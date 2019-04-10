7ce24f379d34af45568b8c6a774c5178744b74ef
Boel <boel.brandstrom@gmail.com>
2019-04-10 08:41:35 +0200

    CHANGED some of the html/css/jss

--- src/frontend/pag2.js
+++ src/frontend/pag2.js
@@ -3,4 +3,14 @@ function listenToFractal() {
   audio.play();
 }
 
+function show_image(src, width, height, alt) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+
+  // This next line will just add it to the <body> tag
+  document.body.appendChild(img);
+}
 function main() {}
